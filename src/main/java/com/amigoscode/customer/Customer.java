5b2b4d84298489c59f83bf27ac28c9f30d9aae43
Kene Ijeh <keneijeh760@gmail.com>
2026-01-18 00:11:34 +0100

    new backend commit

--- src/main/java/com/amigoscode/customer/Customer.java
+++ src/main/java/com/amigoscode/customer/Customer.java
@@ -220,6 +220,8 @@ public class Customer implements UserDetails {
                 ", email='" + email + '\'' +
                 ", age=" + age +
                 ", gender=" + gender +
+                ",  password='" + password + '\'' +
+                ", profileImageId='" + profileImageId + '\'' +
                 '}';
     }
 }
